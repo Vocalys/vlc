3bf67f2720b2db142568473eebbd6d3a2d8c534e
Quentin Rufin <quentin.rufin@epitech.eu>
2014-10-28 15:29:09 +0100

    fix Media file bug by renaming media file and media folder

--- modules/gui/qt4/vocalys_media_file.cpp
+++ modules/gui/qt4/vocalys_media_file.cpp
@@ -1,27 +1,27 @@
-#include "media_file.hpp"
+#include "vocalys_media_file.hpp"
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string.hpp>
 
 static const std::string defaultDelimiters = " .-";
 
-MediaFile::MediaFile(std::string const& fullPath)
+VocalysMediaFile::VocalysMediaFile(std::string const& fullPath)
 	: _path(fullPath)
 {
 	_fileName = boost::filesystem::path(fullPath).stem().string();
 }
 
-MediaFile::MediaFile(MediaFile const& mediaFile)
+VocalysMediaFile::VocalysMediaFile(VocalysMediaFile const& mediaFile)
 {
 	this->_fileName = mediaFile._fileName;
 	this->_path = mediaFile._path;
 }
 
-std::string			MediaFile::fileNameWithoutExtention() const
+std::string			VocalysMediaFile::fileNameWithoutExtention() const
 {
 	return _fileName;
 }
 
-std::list<std::string>	MediaFile::getKeywords() const
+std::list<std::string>	VocalysMediaFile::getKeywords() const
 {
     std::list<std::string> line_parts;
     boost::split(line_parts, this->_fileName, boost::is_any_of(defaultDelimiters));
